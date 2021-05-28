2f070505a15f9d280ceadd50989e96c2f9532bb0
nfcarbone <nfcarbone@gmail.com>
2021-05-28 13:38:34 -0400

    basic routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
+import { BrowserRouter, Redirect, Route } from 'react-router-dom';
+
+const PATHS = {
+  BASE: '/',
+  SEARCH: '/search',
+  RESULTS: '/results',
+  FAVORITES: '/favorites',
+  INSTITUTION: '/institution/',
+};
+PATHS.START = PATHS.SEARCH;
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <BrowserRouter basename='/'>
+      <Route path={PATHS.BASE} exact render={() => <Redirect to={PATHS.START} />} />
+      <Route path={PATHS.SEARCH} component={Search} />
+      <Route path={PATHS.RESULTS} component={Results} />
+      <Route path={PATHS.FAVORITES} component={Favorites} />
+      <Route path={`${PATHS.INSTITUTION}:id`} component={Institution} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
+
+function Search() {
+  return 'Search';
+}
+
+function Results() {
+  return 'Results';
+}
+
+function Favorites() {
+  return 'Favorites';
+}
+
+function Institution() {
+  return 'Institution';
+}
